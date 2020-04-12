0f6f54555f9a7f1b4ae45575ad9dee016b22029e
mupparaju-jk <jayam.krishna@gmail.com>
2020-04-12 13:20:50 +0530

    java apps for practice

---
+++ JavaApp/src/com/jk/test/programs/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.jk.test.programs;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Helloworld");
+	}
+
+}
